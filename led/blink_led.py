0bd4516f3cf3b1a9dd929ccafa6bb1a9e651a20f
sonnhfit <sonnhfit@gmail.com>
2021-01-03 17:27:02 +0700

    Update blink_led.py

--- led/blink_led.py
+++ led/blink_led.py
@@ -2,14 +2,14 @@ import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 
 from time import sleep # Import the sleep function from the time module
 
-
+PIN_GPIO = 11
 GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW) # Set pin 8 to be an output pin and set initial value to low (off)
+GPIO.setup(PIN_GPIO, GPIO.OUT, initial=GPIO.LOW) # Set pin 8 to be an output pin and set initial value to low (off)
 
 
 while True: # Run forever
-  GPIO.output(8, GPIO.HIGH) # Turn on
+  GPIO.output(PIN_GPIO, GPIO.HIGH) # Turn on
   sleep(1) # Sleep for 1 second
-  GPIO.output(8, GPIO.LOW) # Turn off
+  GPIO.output(PIN_GPIO, GPIO.LOW) # Turn off
   sleep(1) # Sleep for 1 second
